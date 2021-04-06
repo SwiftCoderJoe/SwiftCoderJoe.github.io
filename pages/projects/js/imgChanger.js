ed59aae53b89d418716b3f5fb08ff412523e8cf1
SwiftCoderJoe <coderjoe05@gmail.com>
2021-04-06 19:43:10 -0400

    scroll updates! they don't work

--- pages/projects/js/imgChanger.js
+++ pages/projects/js/imgChanger.js
@@ -1,16 +1,15 @@
-$(window).on("scroll touchmove", function() 
-{
-	if ($(document).scrollTop() >= $("#M3K").position().top && $(document).scrollTop() < $("#Aer").position().top  ) 
-	{
-		$('body').css('background-image', 'url(https://4.bp.blogspot.com/-Ivk46EkgQfk/WWjbo4TdJbI/AAAAAAAAFUo/gUD7JABklsIA1gWIr5LS1slyY4QuTMkEwCLcBGAs/s1600/gambar%2Bwallpaper%2Bmobil.jpg)')
-    };
-	if ($(document).scrollTop() >= $("#Aer").position().top && $(document).scrollTop() < $("#Aer2").position().top)
-	{
-		$('body').css('background-image', 'url(https://i1.wp.com/cdn.catawiki.net/assets/marketing/uploads-files/45485-8bdcc8479f93d5a247ab844321b8b9d5e53c49a9-story_inline_image.jpg)')
-    };
-   if ($(document).scrollTop() >= $("#Portfolio").position().top) 
-   {
-		$('body').css('background-image', 'url(https://s-media-cache-ak0.pinimg.com/originals/e1/7a/03/e17a0385726db90de1854177d4af2b4f.jpg)')
-   };
-  
+document.addEventListener('scroll', function() {
+    var newImageUrl = stickyImageDOM.src;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop > 100) {
+       newImageUrl = "../../img/2.jpg"
+    }
+    if (scrollTop > 200) {
+       newImageUrl = "../../img/3.jpg"
+    }
+    if (scrollTop > 300) {
+       newImageUrl = "../../img/4.jpg"
+    }
+
+    stickyImageDOM.src = newImageUrl;
 });
