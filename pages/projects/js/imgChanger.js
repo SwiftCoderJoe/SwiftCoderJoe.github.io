1368c9b14a4a7cb02769436987376fda1e07767d
SwiftCoderJoe <coderjoe05@gmail.com>
2021-04-07 14:10:35 -0400

    Images change

--- pages/projects/js/imgChanger.js
+++ pages/projects/js/imgChanger.js
@@ -1,3 +1,30 @@
+$(window).on("scroll touchmove", function() 
+{
+
+   var newImageUrl = stickyImageDOM.src
+
+	if ($(document).scrollTop() >= $("#M3K").position().top && $(document).scrollTop() < $("#Aer").position().top  ) {
+		newImageUrl = "../../img/avatar.jpg"
+   }
+
+	if ($(document).scrollTop() >= $("#Aer").position().top && $(document).scrollTop() < $("#Aer2").position().top)
+	{
+		newImageUrl = "../../img/2.jpg"
+    };
+   if ($(document).scrollTop() >= $("#Aer2").position().top && $(document).scrollTop() < $("#Portfolio").position().top ) 
+   {
+		newImageUrl = "../../img/3.jpg"
+   };
+   if ($(document).scrollTop() >= $("#Portfolio").position().top) 
+   {
+		newImageUrl = "../../img/4.jpg"
+   };
+  
+   stickyImageDOM.src = newImageUrl
+
+});
+
+/*
 document.addEventListener('scroll', function() {
     var newImageUrl = stickyImageDOM.src;
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -13,3 +40,4 @@ document.addEventListener('scroll', function() {
 
     stickyImageDOM.src = newImageUrl;
 });
+*/
